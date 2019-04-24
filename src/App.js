0dddcb9313270709a6923f282095500749d61891
Estultz731 <elizablue731@gmail.com>
2019-04-24 10:27:21 -0400

    added navbar and promises

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Nav = () =>
+(<nav>
+  <a className="left" href="#blog">Acme Blog</a>
+  <a className="right" href="#social">Social</a>
+  <a className="right" href="#contact">Contact</a>
+</nav>)
 
+const Post = post => 
+(<div className="col-lg-12">
+    <div className="card">
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </div>
+</div>)
+
+class App extends Component {
+
+  state = { loading: true, posts: [] }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(response => this.setState({loading: false, posts: response}))
+  }
+
+  render() { 
+    return (
+      <>
+        <Nav />
+        <div className="container">
+          <div className="row">
+              {this.state.loading && <div>Loading...</div>}
+              {this.state.posts.length > 0 && this.state.posts.map(Post)}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+ 
 export default App;
+
